4ca828b529b87306c8e0b1fb1b3803d0b3b173ec
Grzegorz Błaszczyk <grzegorz.blaszczyk@goodylabs.com>
2016-05-15 12:30:02 +0200

    ADD: you can also define gems to be checked in Gemfile.check file

--- update_gem_in_gemfile.rb
+++ update_gem_in_gemfile.rb
@@ -6,6 +6,7 @@ require 'open-uri'
 class UpdateGemInGemFile 
 
   GEMFILE = "./Gemfile"
+  GEMFILE_CHECK = "./Gemfile.check"
 
   def initialize(gem_names)
     @gem_names = gem_names
@@ -14,8 +15,19 @@ class UpdateGemInGemFile
   def print_usage
     puts ""
     puts "Usage:"
-    puts "#{$0} [gem_name] [another_gem_name]"
+    puts "#{$0} [gem_name] [another_gem_name]\n\n"
+    puts "You can also define gem names in #{GEMFILE_CHECK} file:\n"
+    puts "gem_name_1"
+    puts "gem_name_2"
     puts ""
+  end
+
+  def read_gem_names_from_file
+    gem_names = []
+    File.open(GEMFILE_CHECK).each do |line|
+      gem_names << line.strip
+    end
+    gem_names
   end
 
   def latest_gem_version(gem_name)
@@ -31,7 +43,9 @@ class UpdateGemInGemFile
   end
 
   def perform
-    if @gem_names.nil? || @gem_names.empty?
+    if (@gem_names.nil? || @gem_names.empty?) && File.file?(GEMFILE_CHECK)
+      @gem_names = read_gem_names_from_file
+    elsif @gem_names.nil? || @gem_names.empty?
       print_usage
       exit 1;
     end
